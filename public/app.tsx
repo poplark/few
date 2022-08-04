f9599aacfe27d7680add9204e27d3de1d2a3b33d
poplark <songkang666@126.com>
2022-08-04 16:18:20 +0800

    test: 完善 demo

--- public/app.tsx
+++ public/app.tsx
@@ -21,7 +21,7 @@ export default function App() {
             <MenuItem key="1">一</MenuItem>
             <MenuItem key="2">二</MenuItem>
             <MenuItem key="3">三</MenuItem>
-            {/* <MenuItem key="3" onClick={() => setV(3)}>将 v 置为 3</MenuItem> */}
+            <MenuItem key="4" onClick={() => setV(4)}>将 v 置为 4</MenuItem>
           </Menu>
         </DropdownContent>
       </Dropdown>

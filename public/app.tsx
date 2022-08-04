762504052057a93c186a1d909bf8e367331d7002
poplark <songkang666@126.com>
2022-08-04 14:44:56 +0800

    test: public 中引入 Button, Menu, Dropdown

--- public/app.tsx
+++ public/app.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@/Button';
+import { Menu, MenuItem } from '@/Menu';
+import { Dropdown, DropdownTrigger, DropdownContent } from '@/Dropdown';
 
 export default function App() {
-  return <div>Hello world.</div>;
+  const [v, setV] = useState(0);
+  const handleClick = () => {
+    setV(v+1);
+  }
+  return (
+    <div>
+      <div>Hello world. v: {v} </div>
+      <div><Button onClick={handleClick}>ClickMe</Button></div>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button color="default">hi dropdown</Button>
+        </DropdownTrigger>
+        <DropdownContent>
+          <Menu>
+            <MenuItem key="1">一</MenuItem>
+            <MenuItem key="2">二</MenuItem>
+            <MenuItem key="3">三</MenuItem>
+            {/* <MenuItem key="3" onClick={() => setV(3)}>将 v 置为 3</MenuItem> */}
+          </Menu>
+        </DropdownContent>
+      </Dropdown>
+    </div>
+  )
 }
--- public/index.tsx
+++ public/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import '../src/styles/index.scss';
 
 const rootElement =
   document.getElementById('root') || document.createElement('div');

a75d69322fdc5ec31401fb78c37b8e793b3b9032
poplark <songkang666@126.com>
2022-08-09 15:41:09 +0800

    perf: 优化 Popover

--- src/Popover/popover-content.tsx
+++ src/Popover/popover-content.tsx
@@ -20,28 +20,31 @@ export const PopoverContent: React.FC<
     if (!ref.current) return;
     if (!ref.current.parentElement) return;
 
-    const { width ,height } = ref.current.getBoundingClientRect();
+    // const currentElement = ref.current;
+    const { width, height } = ref.current.getBoundingClientRect();
     const parentElement = ref.current.parentElement;
-    const { top: pTop, left: pLeft, bottom: pBottom, right: pRight } = parentElement.getBoundingClientRect();
+    const { top: pTop, left: pLeft, bottom: pBottom, right: pRight, width: pWidth, height: pHeight } = parentElement.getBoundingClientRect();
 
     // 计算上下空间是否够用
     const bottomOverflow = pBottom + height > window.innerHeight;
     const topOverflow = pTop < height;
 
+    const parent = {
+      addClass: [] as string[],
+      delClass: [] as string[],
+    };
     if (['bottom', 'bottomLeft', 'bottomRight'].includes(ctx.placement)) {
       if (bottomOverflow && pTop >= height) {
         // 转到上面去
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-up').concat('is-up').join(' ');
-      } else {
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-up').join(' ');
+        // parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-up').concat('is-up').join(' ');
+        parent.addClass.push('is-up');
       }
     }
     if (['top', 'topLeft', 'topRight'].includes(ctx.placement)) {
       if (topOverflow && pBottom + height <= window.innerHeight) {
         // 转到下面去
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-up').join(' ');
-      } else {
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-up').concat('is-up').join(' ');
+        // parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-up').join(' ');
+        parent.delClass.push('is-up');
       }
     }
 
@@ -52,24 +55,30 @@ export const PopoverContent: React.FC<
     if (['right', 'bottomRight', 'topRight'].includes(ctx.placement)) {
       if (leftOverflow && (window.innerWidth - pLeft) >= width) {
         // 转成左对齐
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-right').join(' ');
-      } else {
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-right').concat('is-right').join(' ');
+        // parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-right').join(' ');
+        parent.delClass.push('is-right');
       }
     }
 
     if (['left', 'bottomLeft', 'topLeft'].includes(ctx.placement)) {
       if (rightOverflow && pRight >= width) {
         // 转成右对齐
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-right').concat('is-right').join(' ');
-      } else {
-        parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-right').join(' ');
+        // parentElement.className  = Array.from(parentElement.classList).filter((item) => item !== 'is-right').concat('is-right').join(' ');
+        parent.addClass.push('is-right');
       }
+    }
+
+    parentElement.className = Array.from(parentElement.classList).concat(parent.addClass).join(' ');
+    parentElement.className = Array.from(parentElement.classList).filter((item) => !parent.delClass.includes(item)).join(' ');
+    return () => {
+      // 消除前面加/删的 Class 或 Style
+      parentElement.className = Array.from(parentElement.classList).concat(parent.delClass).join(' ');
+      parentElement.className = Array.from(parentElement.classList).filter((item) => !parent.addClass.includes(item)).join(' ');
     }
   }, [ref.current, ctx.visible, ctx.placement]);
 
   useLayoutEffect(() => {
-    relocation();
+    return relocation();
   }, [relocation]);
 
   useEffect(() => {
